d78e0b67762424c430495d1cb929959e1c88e0c0
Namila Rahmani <namilarahmani@gmail.com>
2023-07-25 16:00:57 -0500

    added test-train and R^2 / RMSE calculations

--- linearmixedeffects.R
+++ linearmixedeffects.R
@@ -1,14 +1,33 @@
 library(Matrix)
+library(caret)
 library(lme4)
+library(MuMIn)
 
 dataframe <- read.csv("C:\\Users\\namil\\Documents\\stmi-lab-namila\\combined_dataset")
+dataframe <- na.omit(dataframe)
 dim(dataframe)
 
-convcols <- c(2,3,4,5,6,7,8,9, 10, 11) 
+#filter meals as needed
+#only bfast
+filtered_df <- dataframe[dataframe$Meal.Type == "breakfast", ]
+#breakfast and lunches
+filtered_df <- dataframe[dataframe$Meal.Type != "dinner", ]
+
+convcols <- c(6,7,8,9, 10, 11, 12, 13, 14) 
 dataframe[, convcols] <- lapply(dataframe[, convcols], as.numeric)
 
-model <- lmer(peakheight ~ calories + carbs + protein + fat + (1|participant_id), data = dataframe )
+set.seed(123)  # Set a seed for reproducibility
+trainIndex <- createDataPartition(dataframe$dexcom.3hr.auc, p = 0.7, list = FALSE)
+train_data <- dataframe[trainIndex, ]
+test_data <- dataframe[-trainIndex, ]
+
+model <- lmer(peakheight ~ HbA1c + calories + carbs + protein + fat + (1|participant_id), data = train_data)
 summary(model)
 
-predicted <- predict(model, newdata = dataframe)
-plot(predicted, dataframe$peakheight, xlab = "Predicted", ylab = "actual")
+predicted <- predict(model, newdata = test_data)
+actual <- test_data$peakheight
+plot(predicted, actual, xlab = "Predicted", ylab = "actual")
+
+#errors
+rmse <- sqrt(mean((predicted - actual)^2))
+r_squared <- r.squaredGLMM(model)
